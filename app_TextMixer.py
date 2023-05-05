9fa06842e75e0d572e45521eead9cbedcd887b2d
SARTORIS Jean-Paul.s.i <116564785+Mir0um@users.noreply.github.com>
2023-05-05 19:56:09 +0200

    Add files via upload

--- app_TextMixer.py
+++ app_TextMixer.py
@@ -1,17 +1,17 @@
-import json
-import os
-import requests
 import streamlit as st
-import base64
-import time
-import datetime
-import tempfile
 from gtts import gTTS
+import datetime
+import requests
+import tempfile
+import base64
+import json
+import time
 import uuid
+import os
+
 
 def generate_uuid():
     return uuid.uuid4()
-
 
 
 with open('config.json', 'r', encoding='utf-8') as file:
@@ -21,13 +21,15 @@ text_app = config["text_app"]
 text_ifo = config["text_ifo"]
 lang = config["lang"]
 
+
 def sec_to_min_sec(seconds):
     if seconds < 60:
-        return (f":green[{round(seconds,2)}] secondes")
+        return f":green[{round(seconds, 2)}] secondes"
     else:
         minutes = seconds // 60
         remaining_seconds = seconds % 60
-        return (f":green[{round(minutes)}] minutes et :green[{round(remaining_seconds,2)}] secondes")
+        return f":green[{round(minutes)}] minutes et :green[{round(remaining_seconds, 2)}] secondes"
+
 
 def text_to_audio(text, lang):
     tts = gTTS(text, lang=lang)
@@ -35,10 +37,14 @@ def text_to_audio(text, lang):
         temp_file = fp.name
     tts.save(temp_file)
     return temp_file
+
+
 def audio_player(audio_file):
     audio_bytes = open(audio_file, "rb").read()
     encoded_audio = base64.b64encode(audio_bytes).decode()
-    st.markdown(f'<audio controls autoplay src="data:audio/mp3;base64,{encoded_audio}" > </audio>', unsafe_allow_html=True)
+    st.markdown(f'<audio controls autoplay src="data:audio/mp3;base64,{encoded_audio}" > </audio>',
+                unsafe_allow_html=True)
+
 
 def translate(text, target_language, source=None):
     if source is None:
@@ -61,34 +67,33 @@ def translate(text, target_language, source=None):
     else:
         return None
 
-def transformations(user_text,mod):
-    user_text_old =user_text
+
+def transformations(user_text, mod, formatted_date_time):
+    start_time = time.time()
+
+    user_text_old = user_text
     now = datetime.datetime.now()
-    formatted_date_time = now.strftime("%d/%m/%Y %H:%M:%S")
 
     start_time = time.time()
-    print(formatted_date_time, [mod[5:11]] , (user_text if len(user_text) <= 50 else user_text[:50] + "..."))
 
     if mod != "Mode normal.":
-        histo = {}
-        histo["start"] = {}
+        histo = {"start": {}}
         histo["start"]["time"] = time.time() - start_time
         histo["start"]["fr"] = user_text
         histo["start"]["len"] = len(histo["start"]["fr"])
 
     old_lang = "fr"
     progress_bar = st.progress(0)
-    progress_bar.progress(0, text="0.00%")  # text="Cela peut prendre quelques minutes.")
+    progress_bar.progress(0, text="0.00%")
 
     for count, i in enumerate(list(lang.keys())):
 
         user_text = translate(user_text, i, old_lang)
 
-        progress = 100 - (((len(list(lang.keys())) - count) / len(list(lang.keys()))) * 100 )
-        #print(formatted_date_time,round(progress,3),'%', end="\r")
-        promt = str(round(progress,2)) +"%"
-        progress_bar.progress(round(progress),text=promt )
-
+        progress = 100 - (((len(list(lang.keys())) - count) / len(list(lang.keys()))) * 100)
+        # print(formatted_date_time,round(progress,3),'%', end="\r")
+        promt = str(round(progress, 2)) + "%"
+        progress_bar.progress(round(progress), text=promt)
 
         if mod != "Mode normal.":
             histo[lang[i]] = {}
@@ -100,31 +105,33 @@ def transformations(user_text,mod):
             histo[lang[i]]["len"] = len(histo[lang[i]]["french"])
         old_lang = i
 
+    st.balloons()
+
     user_text = translate(user_text, "fr")
 
     st.success('Traitement terminé!')
     st.header("resulta:")
 
     audio_file = text_to_audio(user_text, "fr")
-    audio_player(audio_file)
+    st.audio(audio_file,format="audio/wav")
     os.remove(audio_file)
+    st.balloons()
 
     st.write(str(user_text))
 
-
     st.write("---")
     if mod != "Mode normal.":
-
         end_time = time.time()
         histo["time"] = {}
         histo["time"]["start"] = start_time
         histo["time"]["end"] = end_time
         histo["time"]["total"] = end_time - start_time
 
-        st.write("Statistique de la transformation:",histo)
+        st.write("Statistique de la transformation:")
+        st.json(histo)
         st.write("---")
 
-    st.write("Temps de traitement :",sec_to_min_sec(time.time() - start_time),"s")
+    st.write("Temps de traitement :", sec_to_min_sec(time.time() - start_time), "s")
     progress_bar.progress(100)
 
     with open("Log.json", "a", encoding='utf-8') as fichier:
@@ -135,7 +142,7 @@ def transformations(user_text,mod):
         log["output"] = user_text
         log["Executing_time"] = time.time() - start_time
         json_log = json.dumps(log)
-        fichier.write(str(json_log)+"\n")
+        fichier.write(str(json_log) + "\n")
 
 
 def app():
@@ -143,39 +150,85 @@ def app():
 
     user_text = st.text_area("Entrez votre texte ici:", height=350, help=text_app["help"])
 
-    mod = st.radio("Voulez-vous utiliser le mode statistique:", ["Mode normal.", "Mode Statistique."])
+    st.write("Voulez-vous utiliser le mode statistique:")
+    mod = st.checkbox('Mode Statistique.', )
 
-    if mod != "Mode normal.":
-        st.error("Attention, le mode Statistique et activé, le temps de traitement sera plus long.")
-
+    if mod:
+        st.warning("Attention, le mode Statistique et activé, le temps de traitement sera plus long.", icon="⚠️")
+        mod = "Mode statistique."
+    else:
+        mod = "Mode normal."
 
     if st.button("Transformer mon texte."):
         if not any(char.isalpha() or char.isdigit() for char in user_text):
-            st.error("Vous devez entrer un texte")
+            st.info("Vous devez entrer un texte", icon="ℹ️")
         else:
             with st.spinner('Transformation en cours...'):
-                transformations(user_text, mod)
+                now = datetime.datetime.now()
+                formatted_date_time = now.strftime("%d/%m/%Y %H:%M:%S")
 
+                print(formatted_date_time, [mod[5:11]], (user_text if len(user_text) <= 50 else user_text[:50] + "..."))
+                uuid = generate_uuid()
+                try:
+                    transformations(user_text, mod, formatted_date_time)
+                    print(formatted_date_time, "OK")
+                except Exception as error:
+                    erreur = {}
+                    erreur["erreur"] = {}
+                    erreur["erreur"]["uuid"] = str(uuid)
+                    erreur["erreur"]["start_time"] = formatted_date_time
+                    now = datetime.datetime.now()
+                    errer_tim = now.strftime("%d/%m/%Y %H:%M:%S")
+                    erreur["erreur"]["errer_tim"] = str(errer_tim)
+                    erreur["erreur"]["text"] = user_text
+                    erreur["erreur"]["mode"] = mod
+                    erreur["erreur"]["erre"] = str(error)
+                    print(f"ERREUR {formatted_date_time} to {uuid},cause : {error}")
+                    with open("error_lod.json", "a", encoding='utf-8') as fichier:
+
+                        fichier.write(str(erreur) + "\n")
+
+                    st.error(f"Une erreur s'est produite le {erreur['erreur']['errer_tim']} nous nous excusons Pour la gêne occasionnée; Voici Ce qui s'est passé : \":blue[{error}]\" ; Identifiant du suivi de l'erreur: \":blue[{uuid}]\" ",icon="🚨")
+                    st.write(f"Le code derrière indique le problème rencontré et l'identifiant du suivi indique le code donnez à votre erreur si vous souhaitez en savoir Transmettez l'identifiant de suivi.")
 
 def info():
     st.title("info")
     st.write(text_ifo["temp"])
+    st.write("""\n\n\n\n Avertissement: \n Ce projet est destiné à des fins éducatives et divertisement et ne doit pas être utilisé à des fins malveillantes. Le résultat produit par TextMixeur ne doit pas être monétisé ou être explouter à des fins commerciales.
+
+Contribuer: \n
+Les contributions sont les bienvenues ! Veuillez créer une pull request pour proposer vos modifications.
+
+Auteur: \n
+Ce projet a été créé par :red[Miroum] : https://jp-sartoris.online/ """)
+
+
+
+def test():
+    st.write("test")
+    st.help(st.write("c la m****"))
+
 
 def main():
-    st.header(text_app["title"])
+    col1, col2 = st.columns([1,8])
 
-    pages = {
-        "Application ": app,
-        "info": info,
-    }
+    with col1:
+        st.image("http://89.86.5.13/img/TestMixeur.png", width=70)
+    with col2:
+        st.header(text_app["title"])
 
-    st.sidebar.title("Navigation")
-    selection = st.sidebar.radio("Aller à", list(pages.keys()))
+    tab1, tab2 = st.tabs(["Application", "info"])
 
-    pages[selection]()
+    with tab1:
+        app()
+
+    with tab2:
+        info()
 
     st.write("---")
     st.write(text_app["Contact"])
 
+
 if __name__ == "__main__":
+    st.set_page_config(page_title="TextMixer-git", page_icon="http://89.86.5.13/img/TestMixeur.png")
     main()
